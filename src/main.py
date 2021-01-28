80a105e52bed36756d6f58a5dc2e6841813deffc
MARIA CHACON <E10140@TMVE.local>
2021-01-28 16:59:03 -0400

    add changes for endpoint put/PATCH

--- src/main.py
+++ src/main.py
@@ -61,6 +61,72 @@ def contact_create():
     db.session.commit()
     return "ok", 200
 
+@app.route('/contacts/<int:position>',methods=['DELETE'])
+def delete_contact(position):
+    db.session.delete(Contact.query.get(position))
+    db.session.commit()
+    return '', 204
+
+@app.route('/contacts/<int:position>', methods=['GET'])
+def handle_contact_for_id(position):
+    contact= Contact.query.get(position)
+    if contact is None:
+        return "This contact not exist", 400
+    else:
+        return jsonify(contact.serialize()),200
+
+@app.route('/contacts/<int:position>', methods=['PUT'])
+def update_allcontact(position):
+    body = request.get_json()
+    contact= Contact.query.get(position)
+    if isinstance (body, dict):
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if contact is None:
+            raise APIException("Contact is not in the list", status_code=400)
+        if 'full_name' not in body:
+            raise APIException('You need to specify the username', status_code=400)
+        if 'email' not in body:
+            raise APIException('You need to specify the email', status_code=400)
+        if 'address' not in body:
+            raise APIException('You need to specify the address', status_code=400)
+        if 'phone' not in body:
+            raise APIException('You need to specify the phone', status_code=400)
+    else: return "no es un diccionario"
+    # at this point, all data has been validated, we can proceed to inster into the bd
+    Contact.query.filter(Contact.id==position).update({
+        "email": body['email'],
+        "full_name": body['full_name'],
+        "address": body['address'],
+        "phone": body['phone']
+        })
+    db.session.commit()
+    return "ok", 200
+
+@app.route('/contacts/<int:position>', methods=['PATCH'])
+def upgrade_contact(position):
+    body = request.get_json()
+    contact_to_upgrade = Contact.query.get(position)
+    if isinstance (body, dict):
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if contact_to_upgrade is None:
+            raise APIException("Contact is not in the list", status_code=400)
+        if 'full_name' != None:
+            new_full_name = body['full_name']
+            contact_to_upgrade.full_name = new_full_name
+        if 'email' != None:
+            new_email = body['email']
+            contact_to_upgrade.email = new_email
+        if 'address' != None:
+            new_address = body['address']
+            contact_to_upgrade.address = new_address
+        if 'phone' != None:
+            new_phone = body['phone']
+            contact_to_upgrade.phone = new_phone
+    db.session.commit()
+    return 'Contact update',200
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
